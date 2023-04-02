832f57ef2ab789a870f37e7fe16d2ec7eb4018b1
ArtemBatkov <artembatkov2015@gmail.com>
2023-04-02 14:23:55 -0400

    Branch #1 Version 1.0
    Commit #4
    
    What's new:
    - Add YummyItem model
    - Add initial fields to that model

---
+++ YummyDrop_online_store/Models/YummyItem.cs
@@ -0,0 +1,19 @@
+﻿namespace YummyDrop_online_store.Models
+{
+    /// <summary>
+    /// The class describes an object of a loot box
+    /// </summary>
+    public class YummyItem
+    {
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public double DropChance { get => _dropChance; }
+
+        public decimal Cost { get => _cost; set => _cost = value; }
+
+        private int _id;
+        private string _name;
+        private double _dropChance;
+        private decimal _cost;
+    }
+}
